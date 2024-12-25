81e9300a1eb8f80a6b0fd3e72f6ddfe9b4ea8dc8
khalidwaheli <oukhakhalid@gmail.com>
2024-12-26 00:02:36 +0100

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import {LayoutComponent} from "./pages/layout/layout.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+
+export const routes: Routes = [
+  {
+    path: '',component: LayoutComponent,
+    children: [
+      {path:'',redirectTo: 'dashboard',pathMatch: 'full'},
+      {path:'dashboard',component: DashboardComponent},
+      {path:'login',component: LoginComponent},
+      {path:'register',component: RegisterComponent},
+    ]
+  }
+];
